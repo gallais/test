87ccab31c2d50405c7d50cfa1555ba2d1b5d9b27
gallais <guillaume.allais@ens-lyon.org>
2016-04-18 15:48:22 +0200

    fixed typo

--- install_agda.sh
+++ install_agda.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-export PATH=/$HOME/.cabal/bin:$opt/ghc/7.10.3/bin:/opt/cabal/1.22/bin:$PATH;
 if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "2.5.1" ]; then
   cabal update
   cabal install alex happy
